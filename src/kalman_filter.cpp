d263f6730a62cde447d6f630edb4529078329b05
bguisard <bruno.guisard@gmail.com>
2017-06-12 14:10:29 -0400

    feat: Includes Predict, Update in kalman_filter
    This commit includes the functions to predict new state and also the regular
    Kalman Filter Update for LIDAR observations.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -18,17 +18,22 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  // Kalman Filter update equations
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();    // storing Ht matrix to improve speed
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();       // storing S inverse to improve speed
+  MatrixXd K = P_ * Ht * Si;
+
+  // New Estimate
+  x_ = x_ + (K * y);
+  MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
